047589b2436d03d4575ed94865c6a1b1273cb386
alan <alan@alans-Mac.local>
2015-02-11 18:00:45 -0800

    Initial Commit

---
+++ COMP4977_NavController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COMP4977_NavController
+//
+//  Created by alan on 2/11/15.
+//  Copyright (c) 2015 alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
